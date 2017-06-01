444b7e2fea057a41761b28fa5bb791d3599076dd
TuanAnh Nguyen <TuanAnh Nguyen>
2017-06-01 09:42:55 +0700

    Add project files.

---
+++ Weather/src/app/app.module.ts
@@ -0,0 +1,25 @@
+﻿import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import {WeatherDetailPage} from '../pages/WeatherDetail/WeatherDetail'
+import { WeatherProvider } from '../providers/Weather'
+
+@NgModule({
+    declarations: [
+        MyApp,
+        HomePage,
+        WeatherDetailPage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        WeatherDetailPage
+    ],
+    providers: [WeatherProvider, { provide: ErrorHandler, useClass: IonicErrorHandler }]
+})
+export class AppModule { }
---
+++ Weather/src/pages/WeatherDetail/WeatherDetail.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the WeatherDetail page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+    selector: 'page-WeatherDetail',
+    templateUrl: 'WeatherDetail.html'
+})
+export class WeatherDetailPage {
+
+    forecast: any;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        //Pull the selected forecast off of navParams
+        this.forecast = this.navParams.get('forecast');
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad WeatherDetailPage');
+    }
+
+}
